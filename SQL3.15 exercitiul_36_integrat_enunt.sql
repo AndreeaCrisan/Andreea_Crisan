25a36f50042557dd20f011e4dcc51c8e51542e5c
AndreeaCrisan <54353817+AndreeaCrisan@users.noreply.github.com>
2022-05-08 16:31:54 +0200

    Add files via upload

---
+++ SQL3.15 exercitiul_36_integrat_enunt.sql
@@ -0,0 +1,48 @@
+	 -- # --  Exercitii JOIN, subinterogari, variabile  -- # --
+		####################################################
+        
+USE orase;
+-- Initializati variabile cu valoarea Romania, Maramures si Europe, pe care le veti utiliza
+SET @R = '', @M = '', @E = '';
+        
+-- aflati numele tuturor regiunile (judetelor) din Romania?
+
+
+-- aflati numele tuturor localitatilor din Romania si numele judetului de care apartin?
+	# (fara admin_code sub)
+    
+
+-- care sunt localitatile din judetul Maramures (fara admin_code sub)?
+
+
+-- care sunt tarile cu peste 100 de localitati ordonate dupa numar loc desc?
+
+
+-- care este numarul de orase pentru fiecare judet din Romania? (doua coloane: NumeJudet si NumarOrase)
+
+ 
+-- care sunt tarile fara orase din Africa? (left join)
+    
+    
+-- care sunt orasele din cele mai populate 3 tari?
+    # 1 - extragem primele trei tari dupa populatie (ordonate desc limit 3);
+	# 2 - aflam orasele din tarile cu id-urile celor 3;
+
+
+-- orasele din tarile de pe continentele americane care au moneda USD, EXCEPTAND USA    
+
+    
+-- orasele din tarile cu denumire de insula (oras, tara)
+
+  
+-- numarul de orase pentru fiecare regiune, tara, continent 
+
+
+-- Orasele de pe paralela 45 din Romania  
+
+
+USE magazin;
+-- extrageti lista cu vanzarile de legume din luna mai 2020 (produs, cantitate, pret, data) - (produse, vanzari, categorii)
+
+
+
